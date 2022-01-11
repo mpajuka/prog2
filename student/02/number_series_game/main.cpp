87b4890a3a03707efc97c545c6580f50059ba3a2
Miska Pajukangas <miska.pajukangas@tuni.fi>
2022-01-11 17:17:11 +0200

    Reading input and printing numbers

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,6 +4,11 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int numbers = 0;
+    std::cin >> numbers;
+
+    for ( int counter = 1; counter < numbers + 1; ++counter )
+        std::cout << counter << std::endl;
 
     return 0;
 }
