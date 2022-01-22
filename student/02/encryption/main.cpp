1ebcd9dbfe6ff26f03c2f75e929943e4b290cbb4
Miska Pajukangas <miska.pajukangas@tuni.fi>
2022-01-22 18:56:34 +0200

    Added error checking

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -2,8 +2,65 @@
 
 using namespace std;
 
+const int ASCII_MIN = 97;
+const int ASCII_MAX = 122;
+const int KEY_LENGTH = 26;
+
+const std::string ERROR_ALPHABET = "Error! The encryption key must contain all alphabets a-z.";
+const std::string ERROR_CASE = "Error! The encryption key must contain only lower case characters.";
+const std::string ERROR_LENGTH = "Error! The encryption key must contain 26 characters.";
+
+bool check_alphabet(const std::string &key);
+bool check_lower_case(const std::string &key);
+bool check_length(const std::string &key);
+
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    // lue syöte
+
+    std::string key = "";
+    std::string text = "";
+
+    std::cout << "Enter the encryption key: ";
+    std::getline(std::cin, key);
+
+    // testaa avain
+
+    if(!check_length(key) or
+       !check_lower_case(key) or
+       !check_alphabet(key))
+    {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+bool check_alphabet(const std::string &key) {
+    for(uint c = ASCII_MIN; c <= ASCII_MAX; c++) {
+        if(key.find(c) == std::string::npos) {
+            std::cout << ERROR_ALPHABET << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+bool check_lower_case(const std::string &key) {
+    for(uint i = 0; i < key.length(); i++) {
+        char c = key.at(i);
+        if(c < ASCII_MIN or c > ASCII_MAX) {
+            std::cout << ERROR_CASE << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+bool check_length(const std::string &key) {
+    if (key.length() != KEY_LENGTH) {
+        std::cout << ERROR_LENGTH << std::endl;
+        return false;
+    }
+    return true;
 }
