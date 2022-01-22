aa1aa0dd9fae20ab1de00373a52a06800a97198a
Miska Pajukangas <miska.pajukangas@tuni.fi>
2022-01-22 18:00:54 +0200

    fixed output

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -8,13 +8,11 @@ int main()
     int n;
     cin >> n;
 
-
-
     int cube = n * n * n;
     if ( cube/n/n != n) {
         cout << "Error! the cube of " << n << " is not " << cube << endl;
     } else {
-        cout << "The cube of " << n << " is " << cube << endl;
+        cout << "The cube of " << n << " is " << cube  << "."<< endl;
     }
 
     return 0;
