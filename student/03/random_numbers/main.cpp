23124cb19958a576a3dde53b8f24cc90b3b01dfa
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-02-05 14:32:12 +0200

    finish produce_random_numbers function

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -7,6 +7,26 @@ using namespace std;
 void produce_random_numbers(unsigned int lower, unsigned int upper)
 {
     // Implement your function here
+    cout << "Enter a seed value: ";
+    int seed;
+    cin >> seed;
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> number(lower, upper);
+
+    int i = 0;
+    while (i == 0) {
+        cout << endl << "Your drawn random number is " << number(gen)
+             << endl;
+        cout << "Press q to quit or any other key to continue: ";
+        string answer;
+        cin >> answer;
+        if (answer == "q") {
+            break;
+        } else {
+            continue;
+        }
+    }
+
 }
 
 int main()
