50f931b659109ac9e0046615a7ca331dbbdb24d8
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-02-07 13:08:01 +0200

    create class and header files for Book

---
+++ student/03/simple_library/book.cpp
@@ -0,0 +1,17 @@
+#include "book.hh"
+#include <iostream>
+
+Book::Book()
+{
+
+}
+
+Book::Book(std::string author, std::string name)
+{
+
+}
+
+Book::~Book()
+{
+
+}
---
+++ student/03/simple_library/book.hh
@@ -0,0 +1,18 @@
+#ifndef BOOK_HH
+#define BOOK_HH
+#include <iostream>
+
+class Book
+{
+public:
+    Book();
+
+    Book(std::string author, std::string name);
+
+
+    ~Book();
+private:
+
+};
+
+#endif // BOOK_HH
