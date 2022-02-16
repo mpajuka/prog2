4f64f66b266a87c9c13712d63648f15527ca2a04
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-02-16 19:32:15 +0200

    Finished line_numbers

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
-using namespace std;
+#include <fstream>
+#include <string>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string input_file;
+    std::string output_file;
+
+    std::cout << "Input file: ";
+    getline(std::cin, input_file);
+
+    std::cout << "Output file: ";
+    getline(std::cin, output_file);
+
+    std::ifstream file_object(input_file);
+    std::ofstream output(output_file);
+    if (not file_object)
+    {
+        std::cout << "Error the file " << input_file << " cannot be opened." << std::endl;
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        int row_count = 1;
+        std::string line;
+        while (getline(file_object, line))
+        {
+            output << row_count << " " << line << std::endl;
+            row_count++;
+        }
+        std::cout << "Success!" << std::endl;
+    }
+
     return 0;
 }
